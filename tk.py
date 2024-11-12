df54ffa705a2e04422e3f6f6845853bd49298ef9
kay <info@beckmann-md.de>
2024-11-12 20:37:25 +0100

    Übung mit Label

--- tk.py
+++ tk.py
@@ -1,4 +1,15 @@
 import tkinter as tk
 
 root = tk.Tk()
+root.title("Hallo Tkinter")
+#root.geometry("400x200")
+
+label = tk.Label(root,
+								text = "Hallo Label",
+								bg="yellow",
+								font=("Comic Sans MS", 15),
+								wraplength=30,
+								justify="right")
+label.pack(padx=100, pady=50)
+
 root.mainloop()
